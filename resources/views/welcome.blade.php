32d3201447a5693990b3578d67df5516a22869eb
Dipesh Khanal <dipeshkhanal79@gmail.com>
2025-12-08 18:50:17 +0545

    feat: gsc and ga setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <x-turnstile.scripts />
+    <x-turnstile.scripts/>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog CMS - Effortless Blog Management with SEO & Control</title>
@@ -57,6 +57,24 @@
           "url": "{{config('app.url')}}"
         }
     </script>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-K1SEF6PY3M"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'G-K1SEF6PY3M');
+    </script>
+    {{--Google Search Console--}}
+    <meta name="google-site-verification" content="XjHLWJOXkGoiX70YdLCYsxreAq0tUi3dI9PGa0Z4lA4"/>
+
+
 </head>
 <body class="font-sans text-gray-800 bg-gray-50">
 <x-navbar/>
